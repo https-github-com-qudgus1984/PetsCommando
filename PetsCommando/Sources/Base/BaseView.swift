6dadb574574bef298b4eeed6bdd9bcc2d7fb826a
cody <qudgus1984@naver.com>
2023-02-06 16:08:18 +0900

    [Add] 기본 폴더 세팅 추가

---
+++ PetsCommando/Sources/Base/BaseView.swift
@@ -0,0 +1,8 @@
+//
+//  BaseView.swift
+//  PetsCommando
+//
+//  Created by 이병현 on 2023/02/06.
+//
+
+import Foundation
---
+++ PetsCommando/Sources/Model/Realm/RealmModel.swift
@@ -0,0 +1,8 @@
+//
+//  RealmModel.swift
+//  PetsCommando
+//
+//  Created by 이병현 on 2023/02/06.
+//
+
+import Foundation
