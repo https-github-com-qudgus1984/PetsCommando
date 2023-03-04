6871f5901a83050cdefb10c9407c319b282d01fa
cody <qudgus1984@naver.com>
2023-03-04 18:50:54 +0900

    [Add] Font 적용 확인 및 Coordinator 적용해 화면 전환 확인 완료

--- PetsCommando/Sources/Presents/Auth/Login/LoginView/LoginViewController.swift
+++ PetsCommando/Sources/Presents/Auth/Login/LoginView/LoginViewController.swift
@@ -8,9 +8,7 @@
 import UIKit
 import SnapKit
 
-//protocol LoginControllerDelegate {
-//    func checkNickname()
-//}
+
 final class LoginViewController: BaseViewController {
     
     //MARK: UI
@@ -21,45 +19,46 @@ final class LoginViewController: BaseViewController {
     
     //MARK: Delegate
     weak var coordinator: LoginCoordinator?
-//    var delegate: LoginControllerDelegate?
-
     
+    override func loadView() {
+        self.view = CheckNicknameView()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
-    override func setupAttributes() {
-        super.setupAttributes()
-        scrollView.isScrollEnabled = false
-
-    }
-    
-    override func setupLayout() {
-        view.addSubview(scrollView)
-        scrollView.snp.makeConstraints { make in
-            make.edges.equalTo(view)
-        }
-        
-        scrollView.addSubview(contentView)
-        contentView.snp.makeConstraints { make in
-            make.top.bottom.equalTo(scrollView)
-            make.leading.trailing.equalTo(view)
-            make.width.height.equalTo(view)
-        }
-        
-        contentView.addSubview(idTextField)
-        idTextField.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview().inset(16)
-            make.top.equalToSuperview()
-        }
-        
-        contentView.addSubview(passwordTextField)
-        passwordTextField.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview().inset(16)
-            make.top.equalTo(idTextField.snp.bottom).offset(16)
-        }
-    }
+//    override func setupAttributes() {
+//        super.setupAttributes()
+//        scrollView.isScrollEnabled = false
+//
+//    }
+//
+//    override func setupLayout() {
+//        view.addSubview(scrollView)
+//        scrollView.snp.makeConstraints { make in
+//            make.edges.equalTo(view)
+//        }
+//
+//        scrollView.addSubview(contentView)
+//        contentView.snp.makeConstraints { make in
+//            make.top.bottom.equalTo(scrollView)
+//            make.leading.trailing.equalTo(view)
+//            make.width.height.equalTo(view)
+//        }
+//
+//        contentView.addSubview(idTextField)
+//        idTextField.snp.makeConstraints { make in
+//            make.horizontalEdges.equalToSuperview().inset(16)
+//            make.top.equalToSuperview()
+//        }
+//
+//        contentView.addSubview(passwordTextField)
+//        passwordTextField.snp.makeConstraints { make in
+//            make.horizontalEdges.equalToSuperview().inset(16)
+//            make.top.equalTo(idTextField.snp.bottom).offset(16)
+//        }
+//    }
 }
 
