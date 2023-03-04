69606856cca4f92d32864a6153bfe583f8b4ccf2
cody <qudgus1984@naver.com>
2023-03-04 15:00:14 +0900

    [Design] Color Hunt Base Color Setting Finish

---
+++ PetsCommando/Sources/Utils/Constants/Color.swift
@@ -0,0 +1,31 @@
+//
+//  Color.swift
+//  PetsCommando
+//
+//  Created by 이병현 on 2023/03/04.
+//
+
+import UIKit
+
+
+struct Color {
+        
+    struct BaseColor {
+        static let white =  #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        static let black =  #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+        
+        static let green =  #colorLiteral(red: 0.2862745098, green: 0.862745098, blue: 0.5725490196, alpha: 1)
+        static let whitegreen =  #colorLiteral(red: 0.8039215686, green: 0.9568627451, blue: 0.8823529412, alpha: 1)
+        static let yellowgreen =  #colorLiteral(red: 0.6980392157, green: 0.9215686275, blue: 0.3803921569, alpha: 1)
+        
+        static let hunt1 = #colorLiteral(red: 0.7977283597, green: 0.6393162608, blue: 0.5133113265, alpha: 1)
+        static let hunt2 = #colorLiteral(red: 0.8901960784, green: 0.7176470588, blue: 0.6274509804, alpha: 1)
+        static let hunt3 = #colorLiteral(red: 0.9294117647, green: 0.8039215686, blue: 0.7333333333, alpha: 1)
+        static let hunt4 = #colorLiteral(red: 1, green: 0.9294117647, blue: 0.8588235294, alpha: 1)
+        
+        static let success =  #colorLiteral(red: 0.3843137255, green: 0.5607843137, blue: 0.8980392157, alpha: 1)
+        static let error =  #colorLiteral(red: 0.9137254902, green: 0.4, blue: 0.4196078431, alpha: 1)
+        static let focus =  #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+    }
+    
+}
