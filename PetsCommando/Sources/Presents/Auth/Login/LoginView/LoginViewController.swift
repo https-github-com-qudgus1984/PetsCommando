1f5e493d77c1ba2442c1b15e26462088370e9207
cody <qudgus1984@naver.com>
2023-03-04 14:42:30 +0900

    [Add] StoryBoard 삭제 및 BaseViewController 세팅 완료

---
+++ PetsCommando/Sources/Presents/Auth/Login/LoginView/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  PetsCommando
+//
+//  Created by 이병현 on 2023/03/04.
+//
+
+import UIKit
+
+class LoginViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
