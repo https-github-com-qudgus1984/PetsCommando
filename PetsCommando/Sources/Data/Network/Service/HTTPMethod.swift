c2548b3d77782e1a2aed9ac7e57bf87313cfc0fe
haha1haka <haha1haka@icloud.com>
2023-03-25 21:12:09 +0900

    practice Generic

--- PetsCommando/Sources/Data/Network/Service/HTTPMethod.swift
+++ PetsCommando/Sources/Data/Network/Service/HTTPMethod.swift
@@ -18,3 +18,47 @@ enum HTTPMethod: String {
 protocol HTTP {
     var path: String { get set }
 }
+
+
+struct Person {
+    let name: String
+}
+
+var people = [
+    Person(name: "사람1"),
+    Person(name: "사람22"),
+    Person(name: "사람333")
+]
+
+func longestName() -> String {
+    return people
+        .sorted(by: { $0.name.count > $1.name.count })
+        .first!
+        .name
+}
+
+func shortestName() -> String {
+    return people
+        .sorted(by: { $0.name.count > $1.name.count })
+        .last!
+        .name
+}
+
+print(longestName())
+print(shortestName())
+
+
+extension Collection where Element == String {
+    func sortedByLength() -> [Element] {
+        return sorted(by: { $0.count < $1.count })
+    }
+}
+
+func gLongestName() -> String {
+    return people
+        .map(\.name)
+        .sortedByLength()
+        .last!
+}
+
+print(gLongestName())
