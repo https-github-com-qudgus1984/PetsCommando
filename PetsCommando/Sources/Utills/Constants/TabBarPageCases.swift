0765f0f8a54c429bbff49a518972066d9ebe1614
cody <qudgus1984@naver.com>
2023-03-31 18:55:11 +0900

    [Add] TabBar Img 나타나도록 구성

--- PetsCommando/Sources/Utills/Constants/TabBarPageCases.swift
+++ PetsCommando/Sources/Utills/Constants/TabBarPageCases.swift
@@ -8,13 +8,14 @@
 import Foundation
 
 enum TabBarPageCase: String, CaseIterable {
-    case home, sesacshop, mypage
+    case home, search, book, community
 
     init?(index: Int) {
         switch index {
         case 0: self = .home
-        case 1: self = .sesacshop
-        case 2: self = .mypage
+        case 1: self = .search
+        case 2: self = .book
+        case 3: self = .community
         default: return nil
         }
     }
@@ -22,8 +23,9 @@ enum TabBarPageCase: String, CaseIterable {
     var pageOrderNumber: Int {
         switch self {
         case .home: return 0
-        case .sesacshop: return 1
-        case .mypage: return 2
+        case .search: return 1
+        case .book: return 2
+        case .community: return 3
         }
     }
 
@@ -31,14 +33,25 @@ enum TabBarPageCase: String, CaseIterable {
         switch self {
         case .home:
             return "홈"
-        case .sesacshop:
-            return "새싹샵"
-        case .mypage:
-            return "내정보"
+        case .search:
+            return "검색"
+        case .book:
+            return "백화사전"
+        case .community:
+            return "커뮤니티"
         }
     }
 
     func tabIconName() -> String {
-        return self.rawValue
+        switch self {
+        case .home:
+            return "house"
+        case .search:
+            return "magnifyingglass"
+        case .book:
+            return "book"
+        case .community:
+            return "pencil.tip.crop.circle"
+        }
     }
 }
