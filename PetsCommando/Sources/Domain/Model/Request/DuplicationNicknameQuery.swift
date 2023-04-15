a3af7d7ce8dc1e1c4b98a1354314094ea120d9c3
cody <qudgus1984@naver.com>
2023-04-15 17:13:43 +0900

    [Add] Nickname Network Setting / 통신 Success

---
+++ PetsCommando/Sources/Domain/Model/Request/DuplicationNicknameQuery.swift
@@ -0,0 +1,12 @@
+//
+//  NicknameDuplication.swift
+//  PetsCommando
+//
+//  Created by 이병현 on 2023/04/15.
+//
+
+import Foundation
+
+struct DuplicationNicknameQuery: Encodable {
+    let email: String
+}
---
+++ PetsCommando/Sources/Domain/Model/Response/DuplicationNickname.swift
@@ -0,0 +1,14 @@
+//
+//  NicknameDuplication.swift
+//  PetsCommando
+//
+//  Created by 이병현 on 2023/04/15.
+//
+
+import Foundation
+
+struct DuplicationNickname {
+    let nickname: String
+    let message: String
+    let timestamp: String
+}
